a0c7c5d75942ca4b6a1683d6bd1e9053f9f1c047
Marco <posta.email3001@gmail.com>
2023-01-31 01:19:19 +0100

    setup files php e scss

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
-import './bootstrap';
+import '~resources/scss/app.scss';
+import.meta.glob([
+    '../img/**'
+])
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            // edit the first value of the array input to point to our new sass files and folder.
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
     ],
+    // Add resolve object and aliases
+    resolve: {
+        alias: {
+            '~resources': '/resources/'
+        }
+    }
 });
